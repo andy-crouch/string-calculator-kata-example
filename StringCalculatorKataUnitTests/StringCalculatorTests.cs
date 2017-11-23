fff6d1276e84b7b476cf9e81e27460e136bef411
Andy Crouch <andy.amcrouch@gmail.com>
2017-11-23 16:41:46 +0000

    Implement Point 1
    This commit addresses point 1 of the kata, adding the initial Add()
    implementation and associated tests.
    
    In this case I have opted to implement a simple test to ensure that the
    method returns an int.

---
+++ StringCalculatorKataUnitTests/StringCalculatorTests.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using StringCalculatorKata;
+
+namespace StringCalculatorKataUnitTests
+{
+    [TestClass]
+    public class StringCalculatorTests
+    {
+        [TestMethod]
+        public void Add_ReturnsAnInt()
+        {
+            var stringCalculator = new StringCalculator();
+
+            var result = stringCalculator.Add("0");
+
+            Assert.IsInstanceOfType(result, typeof(int));
+        }
+    }
+}
